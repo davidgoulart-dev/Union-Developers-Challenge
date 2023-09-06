4b11264384f165cdf024c92461d56a177e87eb90
David Goulart <davidgoulart.tae@gmail.com>
2023-09-06 17:06:02 -0300

    feat: api ajustada para receber results como parametro e retornat somente 1 resultado, caso seja configurado u numero no componente UsersTable

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,8 +2,12 @@ import axios from "axios";
 
 const BASE_URL = "https://randomuser.me/api/";
 
-async function getUsers()  {
-    const response = await axios.get(`${BASE_URL}`);
+async function getUsers(results: number = 1)  {
+    const response = await axios.get(`${BASE_URL}`, {
+        params: {
+            results
+        }
+    });
 
     return response.data;
 
