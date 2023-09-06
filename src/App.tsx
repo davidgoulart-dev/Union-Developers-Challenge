bb65618d2b26c6c520367325e4aa0d15a8513c2b
davidgoulart-dev <davidgoulart.tae@gmail.com>
2023-09-05 22:47:33 -0300

    feat(components- UserTable.tsx): <thead> e <tbody> criação do HTML do componente UsersTable.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,9 @@ function App() {
   return (
     <>
      <h1>List Users</h1>
+     <table>
+      
+     </table>
     </>
   )
 }
---
+++ src/components/UsersTable/UsersTable.tsx
@@ -0,0 +1,35 @@
+export const UsersTable = ( ) => {
+    return (
+        <>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Title</th>
+            <th>Date</th>
+            <th>Age</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+
+        <tbody>
+           <tr>
+                <td>gdc456dfvdfb</td>
+                <td>Jennie</td>
+                <td>Nicohls</td>
+                <td>Miss</td>
+                <td>11/02/2023</td>
+                <td>30</td>
+                <td>View profile</td>
+           </tr>
+        </tbody>
+
+
+        
+        
+        </>
+    )
+}
+
+export default {UsersTable}
