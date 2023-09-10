26191e6a6b03d0d37f34fbe162ecfafaeed5b5d5
David Goulart <davidgoulart.tae@gmail.com>
2023-09-10 17:06:44 -0300

    feat(api.tsx): criada funcao para buscar usuario através do uuid

--- src/api/api.ts
+++ src/api/api.ts
@@ -13,8 +13,19 @@ async function getUsers(results: number = 1, page: number = 1)  {
     return response.data;
 
 }
+async function getUserByUUID(uuid: string) {
+    const response = await axios.get(`${BASE_URL}`, {
+        params: {
+            uuid: uuid
+        }
+    });
+
+    return response.data.results[0];
+}
+
 
 export const api = {
     getUsers,
+    getUserByUUID
 };
 
