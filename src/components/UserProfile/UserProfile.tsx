c0f5aa31be72c1a02177930e0dc60f2b03213f16
David Goulart <davidgoulart.tae@gmail.com>
2023-09-10 17:26:22 -0300

    Componente UserProfile.tsx com sua estrutura desenvolvida (faltando demais estilizações e informações)

--- src/components/UserProfile/UserProfile.tsx
+++ src/components/UserProfile/UserProfile.tsx
@@ -1,2 +1,40 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { api } from "../../api/api";
 
-  
+
+interface UserParams extends Record<string, string | undefined> {
+    uuid: string;
+}
+
+const UserProfile: React.FC = () => {
+    const { uuid } = useParams<UserParams>();
+
+    const [userData, setUserData] = useState<any>(null);
+
+    useEffect(() => {
+        async function fetchUserData() {
+            if (uuid) {
+                const user = await api.getUserByUUID(uuid);
+                setUserData(user);
+            }
+        }
+
+        fetchUserData();
+    }, [uuid]);
+
+    return (
+        <div>
+            <h1>Perfil</h1>
+            {userData && (
+                <div>
+                    <p>Name: {userData.name.first} {userData.name.last}</p>
+                    <p>Email: {userData.email}</p>
+                </div>
+            )}
+        </div>
+    );
+}
+
+export default UserProfile;
+
--- src/main.tsx
+++ src/main.tsx
@@ -8,13 +8,14 @@ import {
   Route,
 
 } from "react-router-dom";
+import UserProfile from './components/UserProfile/UserProfile.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <React.StrictMode>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/user/:uuid" element={<UserProfile/>}
+        <Route path="/user/:uuid" element={<UserProfile/>}/>
       </Routes>
     </React.StrictMode>
   </BrowserRouter>
